4cd8a86fc2ab497a55e638e360faa7b42c84b015
k.niaz <k.niaz@it22.nl>
2023-09-15 15:09:30 +0500

    Fun-123: isBoolean() created

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const test = (string) => {
 	return string === "test";
 };
 
+const isBoolean = (value) => {
+	return typeof (value) === Boolean;
+}
+
 const test2 = (number) => {
 	return number === 123;
 };
 
-module.exports = { test, test2 };
+module.exports = { test, test2, isBoolean };
